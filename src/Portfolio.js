cb771eea0228d5740b7320349995a92bf5d8d152
Russell Waring <russell.waring@dcmail.ca>
2026-02-18 13:08:28 -0500

    Updated project

--- src/Portfolio.js
+++ src/Portfolio.js
@@ -47,7 +47,8 @@ const Portfolio = () => {
                         "authentication",
                         "docker",
                         "terraform",
-                        "api"
+                        "api",
+                        "comingsoon"
                     ]}
                 />
                 <Project
