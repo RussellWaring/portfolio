fa67b1978cb9353e531883aefb25260453e2bc52
Russell Waring <russell.waring@dcmail.ca>
2026-05-27 09:08:38 -0400

    disabled analytics

--- src/index.js
+++ src/index.js
@@ -3,19 +3,20 @@ import ReactDOM from "react-dom/client";
 import "./index.css";
 import App from "./App";
 
-// Only render analytics in production builds
-if (process.env.NODE_ENV === "production") {
-    const script = document.createElement("script");
-    script.defer = true;
-    script.src = "https://umami-h1mf.onrender.com/script.js";
-    script.setAttribute(
-        "data-website-id",
-        "d0a91f26-a63e-41ca-8bb1-833ccc10e41b",
-    );
-    document.head.appendChild(script);
-}
 
-const root = ReactDOM.createRoot(document.getElementById("root"));
+// Only render analytics in production builds
+// if (process.env.NODE_ENV === "production") {
+//     const script = document.createElement("script");
+//     script.defer = true;
+//     script.src = "https://umami-h1mf.onrender.com/script.js";
+//     script.setAttribute(
+//         "data-website-id",
+//         "d0a91f26-a63e-41ca-8bb1-833ccc10e41b",
+//     );
+//     document.head.appendChild(script);
+// }
+
+// const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(
     <React.StrictMode>
         <App />
